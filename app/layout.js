fdcac2b0e9af98401b480b73ab67b6312df33d4f
Thiaguin_13 <thiago.tadeu1412@gmail.com>
2026-06-18 16:51:05 -0300

    Correção Erro

--- app/layout.js
+++ app/layout.js
@@ -20,7 +20,29 @@ export default function RootLayout({ children }) {
     <html lang="pt-BR">
       <head> <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5SGH49B5" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript> </head>
       <body>
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5SGH49B5" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+         {/* Google Tag Manager - Script principal */}
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-5SGH49B5');
+            `,
+          }}
+        />
+        
+        {/* Google Tag Manager - Noscript (fallback para navegadores sem JS) */}
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5SGH49B5" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          }}
+        />
+        
+        {children}
         <Menu />
         <main>{children}</main>
         <Footer />
